92f57807ea5855e1fe366d30e1230c6f14688dde
Felipe Pinto <felpin1945@gmail.com>
2021-07-12 10:22:57 -0500

    Juego listo, interacción básica. Queda para poder mejorarlo y dar más interacción

--- juego.js
+++ juego.js
@@ -3,16 +3,23 @@ const color1 = document.getElementById('color1')
 const color2 = document.getElementById('color2')
 const color3 = document.getElementById('color3')
 const color4 = document.getElementById('color4')
+const ULTIMO_NIVEL = 5
 
 class Juego {
     constructor(){
+        this.inicializar = this.inicializar.bind(this)
         this.inicializar()
         this.generarSecuencia()
-        this.siguienteNivel()
+        setTimeout(() => {
+            this.siguienteNivel()                
+        }, 250);
     }
 
     inicializar(){
-        btnEmpezar.classList.add('hide') // A las clases de css de ese botón se añade una que se llama 'hide' definida en el .css
+        this.siguienteNivel = this.siguienteNivel.bind(this)
+        this.elegirColor = this.elegirColor.bind(this)// Para dejar atado el this "clase Juego" al método elegirColor, esto porque si no se usa el eventlistener escucha es el HTML del bloton de color
+        this.toggleBtnEmpezar()
+        
         this.nivel = 1
         this.colores = {
             color1,
@@ -22,13 +29,24 @@ class Juego {
         }
     }
 
+    toggleBtnEmpezar(){
+        let check = btnEmpezar.classList.contains('hide')
+        if (check){
+            btnEmpezar.classList.remove('hide') // A las clases de css de ese botón se remueve una que se llama 'hide' definida en el .css
+        } else {
+            btnEmpezar.classList.add('hide') // A las clases de css de ese botón se añade una que se llama 'hide' definida en el .css
+        }
+    }
+
     generarSecuencia(){
         //Se crea un array de la cantidad como parámetro, se llena de solo valores cero, se mapea y modifica uno a uno con el metodo map. Con la funcnión mat se crean valores aleatorios
-        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+        this.secuencia = new Array(ULTIMO_NIVEL).fill(0).map(n => Math.floor(Math.random() * 4))
     }
 
     siguienteNivel(){
+        this.subnivel = 0
         this.iluminarSecuencia()
+        this.agregarEventosClick()
     }
 
     transformarNumeroAColor(numero){
@@ -45,12 +63,26 @@ class Juego {
         } 
     }
 
+    transformarColorANumero(color){
+        switch(color){ // Como se usa return no se necesita break en cada case
+            case 'color1':
+                return 0
+            case 'color2':
+                return 1
+            case 'color3':
+                return 2
+            case 'color4':
+                return 3
+    
+        } 
+    }
+
     iluminarSecuencia(){
         for (let i= 0; i < this.nivel; i++){
             const color = this.transformarNumeroAColor(this.secuencia[i])
-        setTimeout(() => {
-            this.iluminarColor(color)
-        }, 750 * i ); 
+            setTimeout(() => {
+                this.iluminarColor(color)
+            }, 750 * ( i+1 ) ); 
         }
     }
 
@@ -64,9 +96,61 @@ class Juego {
     }
 
     apagarColor(color){
-        console.log(color)
         this.colores[color].classList.remove('light')
     }
+
+    agregarEventosClick(){
+        this.colores.color1.addEventListener('click', this.elegirColor)
+        this.colores.color2.addEventListener('click', this.elegirColor)
+        this.colores.color3.addEventListener('click', this.elegirColor)
+        this.colores.color4.addEventListener('click', this.elegirColor)
+    }
+
+    eliminarEventosClick(){
+        this.colores.color1.removeEventListener('click', this.elegirColor)
+        this.colores.color2.removeEventListener('click', this.elegirColor)
+        this.colores.color3.removeEventListener('click', this.elegirColor)
+        this.colores.color4.removeEventListener('click', this.elegirColor)
+
+    }
+
+    elegirColor(ev){ //El metodo AddEventListener llama un parámetro que lo podemos bautizar ev o event, se llama MouseEvent
+        // console.log(ev)
+        // console.log(this)
+        const nombreColor = ev.target.dataset.color
+        const numeroColor = this.transformarColorANumero(nombreColor)
+        this.iluminarColor(nombreColor)
+        if (numeroColor === this.secuencia[this.subnivel]){
+            this.subnivel++
+            if (this.subnivel === this.nivel){
+                this.nivel++
+                this.eliminarEventosClick()
+                if (this.nivel === (ULTIMO_NIVEL + 1)){
+                    this.ganoElJuego()
+                } else {
+                    setTimeout(this.siguienteNivel(), 1000) // Se hace Bind porque setTimeout cambia el this a windows y queremos el this de la clase
+                } 
+            }
+        } else {
+            this.perdioElJuego()
+        }
+    }
+
+    ganoElJuego(){
+        swal("Good job!", "Felicitaciones, ganaste el juego!", "success")
+        .then(() =>{
+            this.inicializar()
+        })
+    }
+
+    perdioElJuego(){
+        swal("Bad!", "Lo lamentamos, perdistes :s", "error")
+        .then(() =>{
+            this.eliminarEventosClick()
+            this.inicializar()
+        })
+    }
+
 }
 
 function empezarJuego() {
